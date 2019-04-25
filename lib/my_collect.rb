bb6b5dc9b9cb69c957b08b74edebfaae01e5a81b
Andrew Silva <andrewsilva9@gmail.com>
2019-04-25 10:54:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++
@@ -1,6 +0,0 @@
-heisman_winners = ["Kyler Murray", "Baker Mayfield", "Lamar Jackson"]
- def first_name(heismans_winners)
-   first_name(heismans_winners).collect do |player|
-     player.split(" ").last
-   end
- end
